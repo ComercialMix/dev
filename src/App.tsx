8240997c0397d838f5d193954d1d1a33690b239c
Comercial Mix <comercialmixinformatica@gmail.com>
2022-10-13 17:23:46 -0300

    1.0

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import './styles/main.css';
+import './styles/styles.css';
+
+function App() {
+  return (
+    <div className="mx-auto flex flex-col items-center">
+      <header className="max-w-[1344px] w-full">
+
+        <nav className="w-full h-12 bg-white">
+        </nav>
+
+        <div className="flex flex-row justify-around items-center my-20">
+
+          <h1 className="text-5xl leading-none text-white font-normal">
+            Meu nome é<br/>
+            <span className="font-black text-6xl text-transparent bg-tc-orange-gradient bg-clip-text">Thiago Côrtes</span><br/>
+            e eu tenho a<br/>
+            <span className="font-black text-6xl text-transparent bg-tc-orange-gradient bg-clip-text">Solução Digital<br/></span>
+            que o seu<br/>
+            negócio precisa!
+          </h1>
+
+          <div className="w-60 h-60 bg-white">
+          </div>
+
+        </div>
+
+
+      </header>
+      <div className="w-full bg-tc-orange-gradient py-4">
+        <div className="w-full bg-tc-orange-gradient2 py-4">
+          <div className="w-full bg-slate-300 py-4">
+            <div className="max-w-[1344px] flex flex-row justify-around items-center">
+
+            <div className="w-60 h-60 bg-white">
+            </div>
+
+            <h1 className="w-150 text-base leading-none text-black font-normal">
+              Sou designer, arte-finalista e desenvolvedor front-end com mais de 4 anos 
+              de experiência, formado em Análise e Desenvolvimento de Sistemas, desenvolvo 
+              Sites, Lojas Virtuais e Landing pages que auxiliam empresas a alcançarem 
+              seus objetivos através do mundo digital.
+            </h1>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html',
+  ],
+  theme: {
+    fontFamily:{
+      sans: ['Inter', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        'tc-blue-gradient': 'linear-gradient(45deg, #1D3B7D 0%, #4370BF 100%)',
+        'tc-orange-gradient': 'linear-gradient(45deg, #F5C630 0%, #E36D32 100%)',
+        'tc-orange-gradient2': 'linear-gradient(45deg, #E36D32 0%, #F5C630 100%)'
+      }
+    },
+  },
+  plugins: [],
+}
