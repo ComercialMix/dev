fdca1d1c5dc76e3e8337b66d60043897d6597dd2
Comercial Mix <comercialmixinformatica@gmail.com>
2022-10-21 18:08:38 -0300

    1.0

--- src/App.tsx
+++ src/App.tsx
@@ -37,10 +37,10 @@ function App() {
 
                 <img src={avatar} className="w-80" alt="" />
                 
-                <div className="w-96">
+                <div className="w-1/3">
                   <h2 className="text-2xl font-bold">Quem sou eu?</h2>
 
-                  <p className="w-96 text-lg leading-none text-black font-normal">
+                  <p className="text-lg leading-none text-black font-normal">
                     Sou designer, arte-finalista e desenvolvedor front-end com mais de 4 anos 
                     de experiência, formado em Análise e Desenvolvimento de Sistemas, desenvolvo 
                     Sites, Lojas Virtuais e Landing pages que auxiliam empresas a alcançarem 
@@ -51,6 +51,46 @@ function App() {
             </div>
 
           </div>
+        </div>
+      </div>
+      
+      <div className="w-full flex justify-center">
+        <div className="w-full max-w-[1200px] flex flex-row justify-around my-10">
+          <div className="w-60 p-4 flex flex-col items-center justify-center rounded-2xl bg-tc-orange-gradient2">
+            <img className="w-20 h-20" src="" alt="" />
+            <p className="text-xl py-4 text-center font-semibold">
+              Lorem ipsum dolor
+            </p>
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ante ipsum, porttitor quis lectus sit amet, efficitur mollis ligula. Donec magna augue, dignissim vitae mollis eu, egestas ac eros. Sed tincidunt sollicitudin ante in consectetur.
+            </span>
+          </div>
+          <div className="w-60 p-4 flex flex-col items-center justify-center rounded-2xl bg-tc-orange-gradient2">
+            <img className="w-20 h-20" src="" alt="" />
+            <p className="text-xl py-4 text-center font-semibold">
+              Lorem ipsum dolor
+            </p>
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ante ipsum, porttitor quis lectus sit amet, efficitur mollis ligula. Donec magna augue, dignissim vitae mollis eu, egestas ac eros. Sed tincidunt sollicitudin ante in consectetur.
+            </span>
+          </div>
+          <div className="w-60 p-4 flex flex-col items-center justify-center rounded-2xl bg-tc-orange-gradient2">
+            <img className="w-20 h-20" src="" alt="" />
+            <p className="text-xl py-4 text-center font-semibold">
+              Lorem ipsum dolor
+            </p>
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ante ipsum, porttitor quis lectus sit amet, efficitur mollis ligula. Donec magna augue, dignissim vitae mollis eu, egestas ac eros. Sed tincidunt sollicitudin ante in consectetur.
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full h-96 bg-tc-orange-gradient2">
+        <div className="w-full flex flex-row justify-center ">
+          <div className="w-[600px] h-60 mx-10 block bg-slate-200"></div>
+          <div className="w-[600px] h-60 mx-10 block bg-slate-400"></div>
+          <div className="w-[600px] h-60 mx-10 block bg-slate-600"></div>
         </div>
       </div>
 
